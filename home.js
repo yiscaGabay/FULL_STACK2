764169e71d5a77596934b3553913b942ed1a481f
yiscaGabay <yigabay@g.jct.ac.il>
2022-12-18 13:25:45 +0200

    c2

--- home.js
+++ home.js
@@ -1,8 +1,23 @@
-//alert("message");
 
-const users=[];
-function subm(){
-    alert("message");
-    
+
+//const users=[];
+
+addEventListener("onclick", createAccount);
+addEventListener("mouseover", myFunction);
+function createAccount(event){
+    var idStr = event.target.id;
+    if(idStr === "createAccount"){
+        alert("createAccount");
+        document.getElementById("createAccount").innerHTML = form2;
+    }
 }
 
+function myFunction(event){
+    var idStr = event.target.id;
+    if(idStr === "createAccount"){
+        document.getElementById("createAccount").innerHTML = form2;
+        alert("myFunction");
+    }
+    //document.getElementById("createAccount").innerHTML = "red";
+    //alert("myFunction");
+}
