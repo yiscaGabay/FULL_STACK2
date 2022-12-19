3f646629013c7234f0d4ecf2faf1510240fbaba5
efratKRYT <epinyan@g.jct.ac.il>
2022-12-19 11:31:20 +0200

    c3

--- home.js
+++ home.js
@@ -1,23 +1,51 @@
+var users ={};
+let userName;
+let password;
 
+let AmountOfUsers=0; 
+addEventListener("click", handleClick);
+function addUser(user, users){
+    users.push(user);
+}
 
-//const users=[];
-
-addEventListener("onclick", createAccount);
-addEventListener("mouseover", myFunction);
-function createAccount(event){
+function handleClick(event){
     var idStr = event.target.id;
-    if(idStr === "createAccount"){
-        alert("createAccount");
-        document.getElementById("createAccount").innerHTML = form2;
+    if(idStr == "createAccount"){
+        document.getElementById('containerLogIn').style.visibility='hidden';
+        document.getElementById('containerSignIn').style.display="block";
+    }
+
+
+    if(idStr == "signIn"){
+            var user={};
+            userName=document.getElementById('nameSignIn').value;
+            password=document.getElementById('passwordSignIn').value;
+            user.userName=userName;
+            user.password=password;
+            user.achivment={};
+            AmountOfUsers++;
+            users[ `user#${AmountOfUsers}`]=user;
+            alert( window.users);
+        }
+
+    if(idStr == "logIn"){
+            userName=document.getElementById('nameSignIn').value;
+            password=document.getElementById('passwordSignIn').value;
     }
 }
 
-function myFunction(event){
-    var idStr = event.target.id;
-    if(idStr === "createAccount"){
-        document.getElementById("createAccount").innerHTML = form2;
-        alert("myFunction");
-    }
-    //document.getElementById("createAccount").innerHTML = "red";
-    //alert("myFunction");
-}
+
+
+
+//addEventListener('load',checkInput);
+// function init(){
+//     alert("init");
+//     document.getElementById('signIn').disabled=true;
+//     password=document.getElementById('passwordSignIn').value;
+// }
+// function checkInput(event){
+//     var idStr = event.target.id;
+//     if(idStr == "signIn"){
+//         password=document.getElementById('passwordSignIn').value;
+//     }
+// }
