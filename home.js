4b9d73b7e82c04dbf274e2e38596081e3c178450
efratKRYT <epinyan@g.jct.ac.il>
2022-12-20 23:41:49 +0200

    c44

--- home.js
+++ home.js
@@ -1,36 +1,39 @@
-var users ={};
+var users = {};
 let userName;
 let password;
 
-let AmountOfUsers=0; 
+let AmountOfUsers = 0;
 addEventListener("click", handleClick);
-function addUser(user, users){
+function addUser(user, users) {
     users.push(user);
 }
 
-function handleClick(event){
+function handleClick(event) {
     var idStr = event.target.id;
-    if(idStr == "createAccount"){
-        document.getElementById('containerLogIn').style.visibility='hidden';
-        document.getElementById('containerSignIn').style.display="block";
+    if (idStr == "createAccount") {
+        document.getElementById('containerLogIn').style.visibility = 'hidden';
+        document.getElementById('containerSignIn').style.display = "block";
     }
 
 
-    if(idStr == "signIn"){
-            var user={};
-            userName=document.getElementById('nameSignIn').value;
-            password=document.getElementById('passwordSignIn').value;
-            user.userName=userName;
-            user.password=password;
-            user.achivment={};
-            AmountOfUsers++;
-            users[ `user#${AmountOfUsers}`]=user;
-            alert( window.users);
-        }
+    if (idStr == "signIn") {
+        var user = {};
+        userName = document.getElementById('nameSignIn').value;
+        password = document.getElementById('passwordSignIn').value;
+        user.userName = userName;
+        user.password = password;
+        user.achivment = {};
+        AmountOfUsers++;
+        users[`user#${AmountOfUsers}`] = user;
+        alert(window.users);
+    }
 
-    if(idStr == "logIn"){
-            userName=document.getElementById('nameSignIn').value;
-            password=document.getElementById('passwordSignIn').value;
+    if (idStr == "logIn") {
+        userName = document.getElementById('nameSignIn').value;
+        password = document.getElementById('passwordSignIn').value;
+    } else {
+
+
     }
 }
 
